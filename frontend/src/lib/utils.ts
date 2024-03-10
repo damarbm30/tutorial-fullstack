ef26152e70e43bfb702565419b10d83d3f0b7a7e
Damar Buana Murti <damarbm30@gmail.com>
2024-03-10 16:33:53 +0700

    feat: handle axios error

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import dayjs from "dayjs";
+import axios from "axios";
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
@@ -9,18 +10,18 @@ export const cn = (...inputs: ClassValue[]) => {
 export const getDays = (month = dayjs().month(), year = dayjs().year()) => {
   const firstDayOfMonth = dayjs().year(year).month(month).startOf("month");
   const lastDayOfMonth = dayjs().year(year).month(month).endOf("month");
-  const date = [];
+  const dateArr = [];
 
   // get previous month days
   for (let i = 0; i < firstDayOfMonth.day(); i++) {
-    date.push({ date: firstDayOfMonth.day(i), currentMonth: false });
+    dateArr.push({ date: firstDayOfMonth.day(i), currentMonth: false });
   }
 
   // get current month days
-  for (let i = 0; i < lastDayOfMonth.day(); i++) {
+  for (let i = firstDayOfMonth.date(); i <= lastDayOfMonth.date(); i++) {
     const currentDate = firstDayOfMonth.date(i);
 
-    date.push({
+    dateArr.push({
       date: currentDate,
       currentMonth: true,
       today:
@@ -30,15 +31,22 @@ export const getDays = (month = dayjs().month(), year = dayjs().year()) => {
 
   // get next month days
   // 42 comes from 7 columns * 6 rows on ui calendar
-  const upcomingDays = 42 - date.length;
+  const upcomingDays = 42 - dateArr.length;
   for (
     // get first day of next month
     let i = lastDayOfMonth.date() + 1;
     i <= lastDayOfMonth.date() + upcomingDays;
     i++
   ) {
-    date.push({ date: lastDayOfMonth.date(i), currentMonth: false });
+    dateArr.push({ date: lastDayOfMonth.date(i), currentMonth: false });
   }
 
-  return date;
+  return dateArr;
+};
+
+export const handleAxiosError = (error: unknown) => {
+  return {
+    data: null!,
+    status: axios.isAxiosError(error) ? error.response?.status || 500 : 500,
+  };
 };
